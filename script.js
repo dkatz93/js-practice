e4e4561786a18e017591789d3582e31581597140
Your Name <your_email@domain.com>
2016-01-26 20:24:10 -0600

    update java

---
+++ script.js
@@ -0,0 +1,7 @@
+var num = prompt("Enter a number","0")
+math.floor((math.random() *50) + 1);
+if(math.random > num)
+{console.log("The computer wins. Sorry!")
+}
+else
+{console.log("You won. Good job!")}
